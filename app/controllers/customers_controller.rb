753991f1cd75747c75c35b1d02b9735abcd50455
atsushi-okamoto <a.okamoto20@gmail.com>
2021-11-10 05:47:26 +0000

    Add: customer/new

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,30 @@
 class CustomersController < ApplicationController
+  before_action :authenticate_admin!
+
+  def new
+    @customer = Customer.new
+  end
+
+  def create
+    @customer = Customer.new(customer_params)
+    @customer.save
+    redirect_to request.referer
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to customer_path(@cusromer)
+  end
+
+private
+
+  def customer_params
+    params.require(:customer).permit(:name, :phone_num, :address, :allergy, :note, :is_active, :age, :book_id)
+  end
+
 end
