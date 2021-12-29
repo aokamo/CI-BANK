929cbc2b0d8a84ab9844e5bc4a872e163dc2f690
atsushi-okamoto <a.okamoto20@gmail.com>
2021-12-29 07:50:52 +0000

    Fix feature destroy of customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,5 @@
 class Customer < ApplicationRecord
-  has_many :books
+  has_many :books, dependent: :destroy
 
   def self.search(keyword)
     where(["name like? OR phone_num like?", "%#{keyword}%", "%#{keyword}%"])
